16d86f4d363bc734cccfe8a30eb17cf2d57f2295
John Yin <john.yin@ebizdesigner.com>
2013-10-10 14:13:42 +1100

    10/10/2013 NEW JS
    REPLACE NEW JS RATHER THAN OLD METHOD

--- html5layout.js
+++ html5layout.js
@@ -3,24 +3,39 @@
  * http://jsfiddle.net/5yQY5/ 
  */
 
-/* to show block style */
-window.onload = function () {
-
+/* add information icon to all Div */
+function addInfoIcon () {
 	var arrDivs = document.getElementsByTagName('div');
 	for (var i=0; i<arrDivs.length; i++ ) {
 		var divItem = arrDivs[i];
 		/* create icon box at corner */
-		divItem.innerHTML = divItem.innerHTML + '<span class="info-icon" onclick="getStyle(this)"></span>';
+		divItem.innerHTML = divItem.innerHTML + '<span class="info-icon"></span>';
 	}
+ }
 
+/* to show div style */
+function addClickListener() {
+	/* add on click listener for each information icon */
+	var arrInfoIcons = document.getElementsByClassName('info-icon');
+	for (var i=0; i<arrInfoIcons.length; i++ ) {
+		if ( arrInfoIcons[i].addEventListener ) {
+			arrInfoIcons[i].addEventListener( 'click', function(evt) { showStyle(evt); }, false );
+		} else if ( arrInfoIcons[i].attachEvent ) { // IE < 9 :(
+			arrInfoIcons[i].attachEvent( 'click', function(evt) { showStyle(evt); }, false );
+		}
+	}
 }
 
-function getStyle (elm) {
-	
-	/* read and display div block styles */
-	var docClasses = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
-			
-	/* get its parent div */
+/* show styles */
+function showStyle(evt) {
+	alert( getStyle(evt.target) );
+}
+
+/* read div's styles */
+function getStyle(elm) {
+	/* read all style class from StyleSheets */
+	var docClasses = document.styleSheets[0].rules || document.styleSheets[0].cssRules;	
+	/* get the parent's div and style classes from class attribute */
 	parentClasses = elm.parentNode.getAttribute('class').split(' ');
 	parentClassContent = ''; // '<div class="info-box">';
 	for (var i=0; i<parentClasses.length; i++) {
@@ -31,8 +46,10 @@ function getStyle (elm) {
 			}
 		}
 	}
-	
-	alert( parentClassContent );
-	
-	return true;
+	return parentClassContent;
+}
+
+window.onload = function () {
+	addInfoIcon();
+	addClickListener();
 }
